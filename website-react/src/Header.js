6be332ac2864a88412cd667cc1ca00787d540803
Sullivan Xiong <sxiong5307@gmail.com>
2021-01-04 11:57:13 -0800

    components

---
+++ website-react/src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+
+
+function Header() {
+  return (
+    <>
+        <div>
+            <div id="web-header-logo">
+                <img src="../public/Logo.png" alt="Quagga Logo"></img>
+            </div>
+            <div id="web-header-title">
+                Quagga
+            </div>
+        </div>
+    </>
+  );
+}
+
+export default Header;
