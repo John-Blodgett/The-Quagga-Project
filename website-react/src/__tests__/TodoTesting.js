7341eb4529e3b18b7a217ae443ededbf7008432c
John-Blodgett <55813162+John-Blodgett@users.noreply.github.com>
2021-01-08 11:04:47 -0800

    added more tests

--- website-react/src/__tests__/TodoTesting.js
+++ website-react/src/__tests__/TodoTesting.js
@@ -1,36 +1,62 @@
 
 var sampleAssignment = {
-    'name': 'HW10',
-    'desc': 'HW10 is blah blah blah',
+    'name': 'HW1',
+    'desc': 'HW1 is blah blah blah',
     'pointsPossible': 10,
     'grade' : 80,
     'className': 'CSC309'
     };
 var sampleAssignment2 = {
-    'name': 'HW20',
-    'desc': 'HW20 Sucks',
+    'name': 'HW2',
+    'desc': 'HW2 is about doing stuff',
     'pointsPossible': 30,
     'grade' : 100,
-    'className': 'CSC309'
+    'className': 'CSC430'
     };
 var sampleAssignment3 = {
-  'name': 'HW21',
-  'desc': 'Fun Assignment',
+  'name': 'HW3',
+  'desc': 'HW is about 309',
   'pointsPossible': 30,
   'grade' : 50,
   'className': 'CSC309'
   };
 
 var todoList = [sampleAssignment, sampleAssignment2, sampleAssignment3]
+const CSC309Classes = [sampleAssignment3, sampleAssignment]
+const CSC430Classes = [sampleAssignment2]
 
-function highestGrade(todoList) {
+function classWithHighestGrade(todoList) {
   const sortedList = todoList.sort((ele1, ele2) => { 
     return ele2['grade'] - ele1['grade'];})
-  return sortedList[0]['grade']
+  return sortedList[0]
+}
+function classWithLowestGrade(todoList) {
+  const sortedList = todoList.sort((ele1, ele2) => { 
+    return ele1['grade'] - ele2['grade'];})
+  return sortedList[0]
 }
 
-test('Sorts the todo assignments', () => {
-  expect(highestGrade(todoList)).toBe(100);
+function singleClassAssignments(todoList, name) {
+const singleClass = todoList.filter((ele1) => { 
+  return ele1['className'] == name;})
+return singleClass
+}
+
+
+test('Finds the highest grade in a given assignments list', () => {
+  expect(classWithHighestGrade(todoList)).toStrictEqual(sampleAssignment2);
+});
+
+test('Finds the lowest grade in a given assignments list', () => {
+  expect(classWithLowestGrade(todoList)).toStrictEqual(sampleAssignment3);
+});
+
+test('Filers for all 309 courses', () => {
+  expect(singleClassAssignments(todoList, "CSC309")).toStrictEqual(CSC309Classes);
+});
+
+test('Filers for all 430 courses', () => {
+  expect(singleClassAssignments(todoList, "CSC430")).toStrictEqual(CSC430Classes);
 });
 
 
