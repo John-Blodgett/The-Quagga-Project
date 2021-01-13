64780bf85478808774cb1653a46bb6ac035be07e
Sullivan Xiong <sxiong5307@gmail.com>
2021-01-13 13:01:08 -0800

    fixed merge

--- website-react/src/Page.js
+++ website-react/src/Page.js
@@ -1,22 +1,14 @@
 import React from 'react';
 //import ReactDOM from 'react-dom';
 import './Page.css';
-<<<<<<< HEAD
-=======
 import Todo from './components/Todo.js'
 import SortBy from './components/SortBy.js'
 import CustomTask from './components/CustomTask.js'
->>>>>>> df1afc8d8213b62b57f22071cdcf4a71cd96cb58
 
 function Page({renderedComponent}) {
   return (
     <>
-<<<<<<< HEAD
       {renderedComponent}
-=======
-      <Faq />
-      {/* <CustomTask /> */}
->>>>>>> df1afc8d8213b62b57f22071cdcf4a71cd96cb58
     </>
   );
 }
