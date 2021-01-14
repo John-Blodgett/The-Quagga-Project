c3f9b7e9d172a3eae346476c5de42611eaaa4f1a
Brandon Sassano <bsassano@calpoly.edu>
2021-01-13 21:22:48 -0800

    About page functionality

---
+++ website-react/src/About.js
@@ -0,0 +1,69 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+import './About.css';
+import logo from './Logo.png';
+//import zebra from './Zebra 5.png'
+//import Todo from './components/Todo.js'
+//import SortBy from './components/SortBy.js'
+console.log(logo);
+function Header() {
+  return(
+  <div id = "web-header-master">
+      <div id = "zebra-logo">
+      <img src={logo} alt="Logo" />
+    </div>
+  </div>)
+}
+function About() {
+  return(
+  <>
+    <h1 style={{textAlign: "center"}}>
+    About Quagga
+    </h1>
+    <div class  = "Titles">
+    <p style={{textAlign: "left"}}>
+        <text style={{fontWeight: "bold",textDecorationLine: "underline", fontSize: 25}}>
+        Who We Are 
+        </text>
+        <br/><br/>
+        <div class = "info">
+          <text>
+            We are California Polytechnic University of San Luis Obispo students.
+          </text>
+        </div>
+        <br/><br/>
+        <text style={{fontWeight: "bold",textDecorationLine: "underline", fontSize: 25}}>
+        What Quagga Is 
+        </text>
+        <br/><br/>
+        <div class = "info">
+          <text>
+            Quagga is an organizer to-do list for canvas that is meant to make the student experience simple and more pleasant.
+          </text>
+        </div>
+        <br/><br/>
+        <text style={{fontWeight: "bold",textDecorationLine: "underline", fontSize: 25}}>
+        Our Goal 
+        </text>
+        <br/><br/>
+        <div class = "info">
+          <text>
+            We want to make the online learning experience for students as easy of a transition as possible.
+          </text>
+        </div>
+        <br/><br/>
+    </p>
+    </div>
+  </>
+  );
+}
+
+function Page() {
+  return (
+    <>
+    <Header />
+      <About />
+    </>
+  );
+}
+export default Page;
