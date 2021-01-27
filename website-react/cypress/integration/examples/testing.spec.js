b2983db44ab4da781b528400cb010ebb9643fe1d
elibakshi <elimbakshi@gmail.com>
2021-01-27 12:57:19 -0800

    Added Eli Test Case for Text in Custom Tag.

--- website-react/cypress/integration/examples/testing.spec.js
+++ website-react/cypress/integration/examples/testing.spec.js
@@ -10,12 +10,25 @@ describe('All Tests', function() {
         
     })
 
-    it('Sullivan\'s Tests', () => {
+    /*it('Sullivan\'s Tests', () => {
         cy.visit("https://localhost:3000/");
 
         cy.get('.AddTag').click()
         cy.get('.TagsTitle').contains('Tags:')
 
         cy.get('.CreateTag').contains('Create Tag').should('have.attr', 'onClick')
+    }) */
+
+    it('Elis Test - Add custom name to a new tag.', function(){
+        cy.visit("http://localhost:3000/")
+
+        cy.get('.AddTag').click()
+        cy.get('.TagsTitle').contains('Tags:')
+        cy.get('.AddTagFrame')
+        cy.get('.CreateNewFrame')
+
+        cy.get('.CustomInput')
+          .type('Urgent')
+          .should('have.value', 'Urgent')
     })
 })
