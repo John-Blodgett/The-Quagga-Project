f969e68298b3f01dfd13ff174065834df7c6f5e3
Sullivan Xiong <sxiong5307@gmail.com>
2021-01-27 12:11:24 -0800

    fix merge conflict and axios dependency

--- website-react/cypress/integration/examples/testing.spec.js
+++ website-react/cypress/integration/examples/testing.spec.js
@@ -8,7 +8,14 @@ describe('All Tests', function() {
  
         cy.get('.AddTagFrame').should('have.attr', 'test')
         
-  
+    })
 
+    it('Sullivan\'s Tests', () => {
+        cy.visit("https://localhost:3000/");
+
+        cy.get('.AddTag').click()
+        cy.get('.TagsTitle').contains('Tags:')
+
+        cy.get('.CreateTag').contains('Create Tag').should('have.attr', 'onClick')
     })
 })
