ae1aaca872be8f015416f53e5c4325a8b0a3e4ce
Sullivan Xiong <sxiong5307@gmail.com>
2021-01-27 12:48:54 -0800

    test

--- website-react/cypress/integration/examples/testing.spec.js
+++ website-react/cypress/integration/examples/testing.spec.js
@@ -11,11 +11,11 @@ describe('All Tests', function() {
     })
 
     it('Sullivan\'s Tests', () => {
-        cy.visit("https://localhost:3000/");
+        cy.visit("http://localhost:3000/");
 
-        cy.get('.AddTag').click()
-        cy.get('.TagsTitle').contains('Tags:')
+        cy.get('.AddTag').click();
+        cy.get('.TagsTitle').contains('Tags:');
 
-        cy.get('.CreateTag').contains('Create Tag').should('have.attr', 'onClick')
+        cy.get('.CreateTag').should('contain', 'Create Tag');
     })
 })
